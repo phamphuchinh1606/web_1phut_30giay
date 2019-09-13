09d76e62c93dd0ed645f9eed441270d4e85e7c67
unknown <phamphuchinh1606@gmail.com>
2019-09-13 22:45:54 +0700

    add

--- resources/views/admin/paymentBill/payment_bill.blade.php
+++ resources/views/admin/paymentBill/payment_bill.blade.php
@@ -93,7 +93,7 @@
             @endcan
             <div class="row">
                 <div class="col-md-8">
-                    <table class="table table-striped table-bordered datatable dataTable no-footer" id="DataTables_Table_0"
+                    <table class="table table-striped table-bordered datatable dataTable no-footer table-overflow-x" id="DataTables_Table_0"
                            role="grid" aria-describedby="DataTables_Table_0_info" style="border-collapse: collapse !important">
                         <thead>
                         <tr role="row">
